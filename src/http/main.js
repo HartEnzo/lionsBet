1e144b49ac12385589f3ec4779953fa8e713aaf9
HartEnzo <enzoguihartmann@gmail.com>
2024-09-19 08:26:40 -0300

    add(+arquivos)

--- src/http/main.js
+++ src/http/main.js
@@ -1,1 +1,13 @@
+import "dotenv/config";
+import e from "express";
+import user_router from "./http/routes/user-roupe.js";
+import roullete_bet_router from "./http/routes/roullete_bet_router.js";
+const app = e();
 
+app.use(e.json());
+app.use("user", user_router.js);
+app.use("/roullete-bet", roullete_bet_router);
+
+app.listen(process.env.API_PORT, () => {
+    console.log("Aplicação rodando");
+});
